56caf116e494be5cf5e1f1c7689e0abe831bdb42
闕立奇 <liche0241@gmail.com>
2022-06-06 17:07:32 +0800

    fix can't add permissions situations

--- elearning_test/auth_init.py
+++ elearning_test/auth_init.py
@@ -1,10 +1,8 @@
 from unicodedata import name
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group, Permission # name 'Permission' is not defined
 
 def permission_object(codename: str):
     return Permission.objects.get(codename=codename)
-
-
 
 
 # create auth group
@@ -12,9 +10,10 @@ print("create auth group")
 student = Group.objects.create(name="student")
 teacher = Group.objects.create(name="teacher")
 
+
 # setting student auth
 print("setting group permission")
-student.permissions.add(permission_object("add_studentlist"))
-student.permissions.add(permission_object("change_studentlist"))
-student.permissions.add(permission_object("delete_studentlist"))
-student.permissions.add(permission_object("view_studentlist"))
+student.permissions.add(Permission.objects.get(codename="add_studentlist"))
+student.permissions.add(Permission.objects.get(codename="change_studentlist"))
+student.permissions.add(Permission.objects.get(codename="delete_studentlist"))
+student.permissions.add(Permission.objects.get(codename="view_studentlist"))
