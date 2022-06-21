b7c2ae1d679c1ca29f3a4cf2accdb11be96b4873
闕立奇 <liche0241@gmail.com>
2022-06-21 16:54:04 +0800

    2nd change dockfile setting

--- elearning_test/django_init.sh
+++ elearning_test/django_init.sh
@@ -14,14 +14,15 @@ echo "initialization web user auth"
 python manage.py shell <auth_init.py
 
 # setting server property file
-cp server_setting.conf /etc/apache2/sites-available
-LOCALHOST_NAME= hostname
-echo "ServerName "+ $LOCALHOST_NAME>> /etc/apache2/apache2.conf
-echo "copy server_setting.conf"
-a2dissite 000-default.conf
-a2ensite server_setting.conf
-service apache2 start
+# cp server_setting.conf /etc/apache2/sites-available
+# LOCALHOST_NAME= hostname
+# echo "ServerName "+ $LOCALHOST_NAME>> /etc/apache2/apache2.conf
+# echo "copy server_setting.conf"
+# a2dissite 000-default.conf
+# a2ensite server_setting.conf
+# service apache2 start
 
 # execute server
+httpd -D FOREGROUND
 # python manage.py runserver 0.0.0.0:8000
 
