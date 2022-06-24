9110f423bce3e8cbb44b7a7244b000d33f344122
闕立奇 <liche0241@gmail.com>
2022-06-24 16:24:59 +0800

    prepare nginx

--- elearning_test/auth_init.py
+++ elearning_test/auth_init.py
@@ -3,21 +3,23 @@ from django.contrib.auth.models import Group, Permission
 
 
 # create auth group
-print("create auth group")
-student = Group.objects.create(name="student")
-teacher = Group.objects.create(name="teacher")
+try :
+    Group.objects.get(name="student")
+except Group.DoesNotExist:
+    print("create student group")
+    student = Group.objects.create(name="student")
+    student.permissions.add(Permission.objects.get(codename="add_studentlist"))
+    student.permissions.add(Permission.objects.get(codename="change_studentlist"))
+    student.permissions.add(Permission.objects.get(codename="delete_studentlist"))
+    student.permissions.add(Permission.objects.get(codename="view_studentlist"))
 
 
-# setting student auth
-print("setting student group permission")
-student.permissions.add(Permission.objects.get(codename="add_studentlist"))
-student.permissions.add(Permission.objects.get(codename="change_studentlist"))
-student.permissions.add(Permission.objects.get(codename="delete_studentlist"))
-student.permissions.add(Permission.objects.get(codename="view_studentlist"))
-
-# setting teacher auth
-print("setting teacher group permission")
-teacher.permissions.add(Permission.objects.get(codename="add_studentlist"))
-teacher.permissions.add(Permission.objects.get(codename="change_studentlist"))
-teacher.permissions.add(Permission.objects.get(codename="delete_studentlist"))
-teacher.permissions.add(Permission.objects.get(codename="view_studentlist"))
+try:
+    Group.objects.get(name="teacher")
+except Group.DoesNotExist:
+    print("create teacher group")
+    teacher = Group.objects.create(name="teacher")
+    teacher.permissions.add(Permission.objects.get(codename="add_studentlist"))
+    teacher.permissions.add(Permission.objects.get(codename="change_studentlist"))
+    teacher.permissions.add(Permission.objects.get(codename="delete_studentlist"))
+    teacher.permissions.add(Permission.objects.get(codename="view_studentlist"))
