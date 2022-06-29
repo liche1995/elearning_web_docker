80c58cabc759dab6198662ed7d8c6c7e1348c623
闕立奇 <liche0241@gmail.com>
2022-06-29 17:03:49 +0800

    docker+njind+django+mysql initinal startup

--- elearning_test/django_init.sh
+++ elearning_test/django_init.sh
@@ -1,17 +1,16 @@
 ##!/bin/bash
-#
-## static file manage
-#echo "excute collect static file"
-#python manage.py collectstatic --noinput
-#
-## database manage
-#echo "excute database migrate"
-#python manage.py makemigrations
-#python manage.py migrate --database=db_manage
-#
-## usergroup initinalize
-#echo "initialization web user auth"
-#python manage.py shell <auth_init.py
-#
-## excute server
-nginx
+
+# static file manage
+echo "excute collect static file"
+python manage.py collectstatic --noinput
+
+# database manage
+echo "excute database migrate"
+python manage.py makemigrations
+python manage.py migrate --database=db_manage
+
+# usergroup initinalize
+echo "initialization web user auth"
+python manage.py shell <auth_init.py
+
+uwsgi --ini /webcode/uwsgi.ini
